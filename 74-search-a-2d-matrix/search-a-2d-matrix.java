d37049ca9c0d0dbdc8a0455189b206e2fd5ea080
Saqib <118735710+saqib12214416@users.noreply.github.com>
2025-08-05 14:07:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (18.09%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,24 +1,63 @@
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
+class Solution{
+    public boolean searchMatrix(int[][] matrix, int target){
         int n=matrix.length;
         int m=matrix[0].length;
-        for(int i=0;i<n;i++){
-            if(matrix[i][0]<=target&&target<=matrix[i][m-1])
-            return bs(matrix[i],target);
+        int low=0;
+        int high=(n*m)-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int row=mid/m;
+            int col=mid%m;
+            if(matrix[row][col]==target)return true;
+            else if(matrix[row][col]<target)low=mid+1;
+            else high=mid-1;
         }
         return false;
     }
-    public boolean bs(int[] matrix,int target){
-        int i=0;
-        int j=matrix.length-1;
-        while(i<=j){
-            int mid=(i+j)/2;
-            if(matrix[mid]==target)return true;
-            if(matrix[mid]<target)i=mid+1;
-            else{
-                j=mid-1;
-            }
-        }
-return false;
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int n=matrix.length;
+//         int m=matrix[0].length;
+//         for(int i=0;i<n;i++){
+//             if(matrix[i][0]<=target&&target<=matrix[i][m-1])
+//             return bs(matrix[i],target);
+//         }
+//         return false;
+//     }
+//     public boolean bs(int[] matrix,int target){
+//         int i=0;
+//         int j=matrix.length-1;
+//         while(i<=j){
+//             int mid=(i+j)/2;
+//             if(matrix[mid]==target)return true;
+//             if(matrix[mid]<target)i=mid+1;
+//             else{
+//                 j=mid-1;
+//             }
+//         }
+// return false;
+//     }
+// }
