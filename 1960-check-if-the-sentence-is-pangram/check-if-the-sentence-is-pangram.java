ea3bcf374549a884e8cae19c9a50cfd8f4636caa
Saqib <118735710+saqib12214416@users.noreply.github.com>
2025-08-18 12:53:08 +0530

    Time: 4 ms (24.57%) | Memory: 41.8 MB (29.76%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean checkIfPangram(String s) {
+        Set<Character> set=new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            set.add(s.charAt(i));
+        }
+        return set.size()==26;
+    }
+}
